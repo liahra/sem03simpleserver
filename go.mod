8fc45cd205ccdd9c866e194c4b6090aa1f225f01
liahra <chrira17@uia.no>
2023-04-19 20:30:50 +0000

    Oppdatert

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/liahra/sem03simpleserver
+
+go 1.18
+
+require github.com/uia-worker/is105sem03 v0.0.0-20230416140235-f3824ac768bb
--- tcp_server.go
+++ tcp_server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"sync"
+	"github.com/uia-worker/is105sem03/mycrypt"
 )
 
 func main() {
@@ -36,7 +37,10 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
+
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switch msg := string(dekryptertMelding); msg {
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
@@ -53,4 +57,5 @@ func main() {
 		}
 	}()
 	wg.Wait()
+
 }
